df3efb81384df6db80c583bfdfbbd2901caaccac
V9-9 <148236657+V9-9@users.noreply.github.com>
2023-10-22 22:21:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/*     ДЗ: Доработать калькулятор, получение от пользователя действия, которое нужно выполнить.
+Можно получить таким образом - если пользователь вводит 1, то это сложение, 2 - разность и т.д.
+Вывести результат действия. Используйте конструкции if -else, case.                          */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите целое число: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Если Вас интересует сложение, то введите цифру 1\nЕсли Вас интересует"
+               + " вычитание, то введите цифру 2\nЕсли Вас интересует умножение, то введите цифру 3"
+               + "\nЕсли Вас интересует деление, то введите цифру 4\nВаш выбор: ");
+        int vybor = scanner.nextInt();
+
+        System.out.print("Введите целое число: ");
+        int b = scanner.nextInt();
+
+
+        switch (vybor) {
+            case 1:
+                int c = a + b;
+                System.out.println("\nСумма введённых Вами чисел: " + c);
+                break;
+            case 2:
+                int d = a - b;
+                System.out.println("\nРазность введённых Вами чисел: " + d);
+                break;
+            case 3:
+                int e = a * b;
+                System.out.println("\nПроизведение введённых Вами чисел: " + e);
+                break;
+            case 4:
+                int f = a / b;
+                System.out.println("\nЧастное введённых Вами чисел: " + f);
+                break;
+            default:
+                System.out.println("\nВы не были внимательны, данная операция не предусмотрена.\nОтличного времени суток! До свидания!");
+        }
+
+        scanner.close();
+    }
+}
